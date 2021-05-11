1af5bb5d224318eb74a8f6358977b09c7f0642c2
Niels Slo <nielssloesen@hotmail.com>
2021-05-11 14:29:59 +0200

    Adjusted footer.php

--- wp-content/themes/inkoppers/footer.php
+++ wp-content/themes/inkoppers/footer.php
@@ -3,7 +3,39 @@
 defined("ABSPATH") || exit;
 ?>
 
-
+<div id="footer-cbds" class="container">
+    <div class="row">
+        <div class="col">
+            <h2 class="footer-h2">Laten we<br />samen werken.</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <a href="mailto:info@quarterback.nl" title="E-mailadres Quarterback">info@quarterback.nl</a>
+            <div class="">Emmaustraat 1</div>
+            <div class="">5492BK Sint-Oedenrode</div>
+        </div>
+        <div class="col-4">
+            <a href="https://www.instagram.com/quarterback.nl/" target="_blank" title="Instagram Quarterback">Instagram</a>
+            <a href="https://nl.linkedin.com/in/danny-siebers" target="_blank" title="LinkedIn Quarterback">LinkedIn</a>
+            <a href="https://www.facebook.com/quarterback.nl/" target="_blank" title="Facebook Quarterback">Facebook</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <a href="http://localhost/cbdsport/projecten" title="Projecten Quarterback">Projecten</a>
+            <a href="http://localhost/cbdsport/over-ons" title="Over Quarterback">Over ons</a>
+            <a href="http://localhost/cbdsport/contact" title="Contact Quarterback">Contact</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>Copyright &copy; 2021 Quarterback</p>
+            <a href="http://localhost/cbdsport/algemene-voorwaarden" title="Algemene voorwaarden">Algemene voorwaarden</a>
+            <a href="http://localhost/cbdsport/algemene-voorwaarden" title="Privacy">Privacy</a>
+        </div>
+    </div>
+</div>
 
 <?php wp_footer(); ?>
 
