cb46d445f9ffd18f2677530566156b9dbc956bc4
Niels Slo <nielssloesen@hotmail.com>
2021-05-12 16:02:04 +0200

    Added HTML Work page

--- wp-content/themes/inkoppers/template-work.php
+++ wp-content/themes/inkoppers/template-work.php
@@ -7,6 +7,48 @@ defined("ABSPATH") || exit;
 get_header();
 ?>
 
-<h1>Test</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2 class="">Wij ontwikkelen concepten en bouwen merken zonder fratsen.</h2>
+            <p class="">We create brands that stand out. We are here to tell a story through visual language & brand identity</p>
+         </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+            <h5 class="">CBD+Sport</h5>
+            <p class="">The smart athletes choice</p>
+        </div>
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+            <h5 class="">CBD+Sport</h5>
+            <p class="">The smart athletes choice</p>
+        </div>
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+            <h5 class="">CBD+Sport</h5>
+            <p class="">The smart athletes choice</p>
+        </div>
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+            <h5 class="">CBD+Sport</h5>
+            <p class="">The smart athletes choice</p>
+        </div>
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+            <h5 class="">CBD+Sport</h5>
+            <p class="">The smart athletes choice</p>
+        </div>
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+            <h5 class="">CBD+Sport</h5>
+            <p class="">The smart athletes choice</p>
+        </div>
+    </div>
+</div>
 
 <?php get_footer();
