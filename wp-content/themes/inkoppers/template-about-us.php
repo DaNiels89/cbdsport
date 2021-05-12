7e0c242aca72e2f00f31c6415dcbaba10126d64e
Niels Slo <nielssloesen@hotmail.com>
2021-05-12 16:28:10 +0200

    Added HTML About Us page

--- wp-content/themes/inkoppers/template-about-us.php
+++ wp-content/themes/inkoppers/template-about-us.php
@@ -7,6 +7,113 @@ defined("ABSPATH") || exit;
 get_header();
 ?>
 
-<?php include "flexible-content.php"; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <h2 class="">Wij ontwikkelen concepten en bouwen merken</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="">We zijn nuchtere mensen die geloven in hard werken en het opbouwen van duurzame relaties. We vinden het leuk om bedrijven er beter uit te laten zien met een geweldig ontwerp en doen er alles aan om een service te bieden die we zelf graag zouden willen ervaren.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p class="">Wat we doen</p>
+            <p class="">We werken nauw samen met startups om hun visie tot leven te brengen, werken samen met groeiende merken om hen te verbinden met de toekomst, en zijn een creatieve sparrings partner voor toonaangevende bedrijven.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h4 class="">Concept</h4>
+            <p class="">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+        </div>
+        <div class="col">
+            <h4 class="">Branding</h4>
+            <p class="">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p></div>
+        <div class="col">
+            <h4 class="">Design</h4>
+            <p class="">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p class="">Wat we doen</p>
+            <h2 class="">Design and development for every stage of your journey.</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <details>
+                <summary>Ontdekken</summary>
+                <p class="">Our main goal is to help enterprise brands uplift their online presence with little to no effort, while still keeping their original roots. We’re big lovers of design sprints & agile methodologies, so every project is taking 2 weeks or less.</p>
+                <p class="">Merk & Identiteit</p>
+                <p class="">Art direction</p>
+                <p class="">Creatief advies</p>
+                <p class="">UX/UI Design</p>
+                <p class="">Website design</p>
+                <p class="">Website development</p>
+            </details>
+            <details>
+                <summary>Ontwerpen</summary>
+                <p class="">Our main goal is to help enterprise brands uplift their online presence with little to no effort, while still keeping their original roots. We’re big lovers of design sprints & agile methodologies, so every project is taking 2 weeks or less.</p>
+                <p class="">Merk & Identiteit</p>
+                <p class="">Art direction</p>
+                <p class="">Creatief advies</p>
+                <p class="">UX/UI Design</p>
+                <p class="">Website design</p>
+                <p class="">Website development</p>
+            </details>
+            <details>
+                <summary>Ontwikkelen</summary>
+                <p class="">Our main goal is to help enterprise brands uplift their online presence with little to no effort, while still keeping their original roots. We’re big lovers of design sprints & agile methodologies, so every project is taking 2 weeks or less.</p>
+                <p class="">Merk & Identiteit</p>
+                <p class="">Art direction</p>
+                <p class="">Creatief advies</p>
+                <p class="">UX/UI Design</p>
+                <p class="">Website design</p>
+                <p class="">Website development</p>
+            </details>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        </div>
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        </div>
+    </div>
+</div>
 
 <?php get_footer();
