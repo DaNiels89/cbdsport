b920fbb790b68544740bdfc38f1d9a68719de653
Niels Slo <nielssloesen@hotmail.com>
2021-05-17 15:01:02 +0200

    Updated front-page and footer

--- wp-content/themes/inkoppers/footer.php
+++ wp-content/themes/inkoppers/footer.php
@@ -5,15 +5,15 @@ defined("ABSPATH") || exit;
 
 <div id="footer" class="container-fluid">
     <div class="row">
-        <div class="col mt-5 mb-4">
+        <div class="col my-4">
             <h2 class="footer-h2">Laten we<br />samen werken.</h2>
         </div>
     </div>
     <div class="row">
         <div class="col-8 footer-contact-info d-flex flex-column">
             <a href="mailto:info@quarterback.nl" title="E-mailadres Quarterback">info@quarterback.nl</a>
-            <p class="">Emmaustraat 1</p>
-            <p class="">5492BK Sint-Oedenrode</p>
+            <p class="p-footer">Emmaustraat 1</p>
+            <p class="p-footer">5492BK Sint-Oedenrode</p>
         </div>
         <div class="col-4 d-flex flex-column">
             <a href="https://www.instagram.com/quarterback.nl/" target="_blank" title="Instagram Quarterback">Instagram</a>
@@ -22,15 +22,15 @@ defined("ABSPATH") || exit;
         </div>
     </div>
     <div class="row my-4">
-        <div class="col lh-175 d-flex flex-column font-jakarta-regular">
+        <div class="col lh-175 d-flex flex-column font-jakarta-regular mb-2">
             <a href="http://localhost/cbdsport/projecten" title="Projecten Quarterback">Projecten</a>
             <a href="http://localhost/cbdsport/over-ons" title="Over Quarterback">Over ons</a>
             <a href="http://localhost/cbdsport/contact" title="Contact Quarterback">Contact</a>
         </div>
     </div>
     <div class="row my-4">
-        <div class="col footer-copyright">
-            <p class="p-footer-copyright pt-4">Copyright &copy; 2021 Quarterback</p>
+        <div class="col footer-copyright mb-5">
+            <p class="p-footer-copyright pt-5">Copyright &copy; 2021 Quarterback</p>
             <a class="a-footer-copyright" href="http://localhost/cbdsport/algemene-voorwaarden" title="Algemene voorwaarden">Algemene voorwaarden</a>
             <a class="a-footer-copyright pb-5" href="http://localhost/cbdsport/algemene-voorwaarden" title="Privacy">Privacy</a>
         </div>
