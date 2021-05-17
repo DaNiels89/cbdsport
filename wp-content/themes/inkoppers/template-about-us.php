ee60fb72f956922e7beb8c264569b0093114126c
Niels Slo <nielssloesen@hotmail.com>
2021-05-17 16:31:43 +0200

    Updated About Us page

--- wp-content/themes/inkoppers/template-about-us.php
+++ wp-content/themes/inkoppers/template-about-us.php
@@ -7,20 +7,54 @@ defined("ABSPATH") || exit;
 get_header();
 ?>
 
-<div class="container-fluid">
+<div class="container-fluid bg-black text-white">
     <div class="row">
-        <div class="col-sm-12 col-md-6">
+        <div class="col-sm-12 col-md-6 my-5">
             <h2 class="">Wij ontwikkelen concepten en bouwen merken</h2>
         </div>
         <div class="col-sm-12 col-md-6">
-            <p class="">We zijn nuchtere mensen die geloven in hard werken en het opbouwen van duurzame relaties. We vinden het leuk om bedrijven er beter uit te laten zien met een geweldig ontwerp en doen er alles aan om een service te bieden die we zelf graag zouden willen ervaren.</p>
+            <p class="mt-4 mb-5">We zijn nuchtere mensen die geloven in hard werken en het opbouwen van duurzame relaties. We vinden het leuk om bedrijven er beter uit te laten zien met een geweldig ontwerp en doen er alles aan om een service te bieden die we zelf graag zouden willen ervaren.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-black py-5">
+    <div class="row">
+        <div class="col">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-black text-white py-5">
+    <div class="row">
+        <div class="col">
+            <p class="p-gray">Wat we doen</p>
+            <p class="mb-5">We werken nauw samen met startups om hun visie tot leven te brengen, werken samen met groeiende merken om hen te verbinden met de toekomst, en zijn een creatieve sparrings partner voor toonaangevende bedrijven.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-black text-white">
+    <div class="row">
+        <div class="col-12">
+            <h4 class="mb-4">Concept</h4>
+            <p class="p-color-light mb-5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+        </div>
+        <div class="col-12">
+            <h4 class="mb-4">Branding</h4>
+            <p class="p-color-light mb-5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+        </div>
+        <div class="col-12 mb-5">
+            <h4 class="mb-4">Design</h4>
+            <p class="p-color-light mb-5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
         </div>
     </div>
 </div>
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col">
+        <div class="col half-black-half-white">
             <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
         </div>
     </div>
@@ -29,43 +63,56 @@ get_header();
 <div class="container-fluid">
     <div class="row">
         <div class="col">
-            <p class="">Wat we doen</p>
-            <p class="">We werken nauw samen met startups om hun visie tot leven te brengen, werken samen met groeiende merken om hen te verbinden met de toekomst, en zijn een creatieve sparrings partner voor toonaangevende bedrijven.</p>
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <h4 class="">Concept</h4>
-            <p class="">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
-        </div>
-        <div class="col">
-            <h4 class="">Branding</h4>
-            <p class="">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p></div>
-        <div class="col">
-            <h4 class="">Design</h4>
-            <p class="">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <p class="">Wat we doen</p>
+            <p class="p-gray mt-5">Wat we doen</p>
             <h2 class="">Design and development for every stage of your journey.</h2>
         </div>
     </div>
+</div>
+
+<div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
 </div>
 
 <div class="container-fluid">
@@ -105,13 +152,17 @@ get_header();
     </div>
 </div>
 
-<div class="container-fluid">
+<div class="container-fluid bg-black text-white pt-5">
     <div class="row">
-        <div class="col">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        <div class="col-12">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
+            <h4 class="mt-4 mb-2">CBD+Sport</h4>
+            <p class="p-gray">The smart athletes choice</p>
         </div>
-        <div class="col">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="">
+        <div class="col-12">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
+            <h4 class="mt-4 mb-2">CBD+Sport</h4>
+            <p class="p-gray">The smart athletes choice</p>
         </div>
     </div>
 </div>
