ca20d6ea6084f16f09bb9c129c19ca3e6b48b7cb
Niels Slo <nielssloesen@hotmail.com>
2021-05-21 13:18:47 +0200

    Small updates header

--- wp-content/themes/inkoppers/header.php
+++ wp-content/themes/inkoppers/header.php
@@ -15,17 +15,18 @@ defined( "ABSPATH" ) || exit;
 <?php do_action("wp_body_open"); ?>
 
 <div id="header" class="container-fluid <?php if (get_field("header_dark")): echo "bg-ink-dark"; endif; ?>">
-    <div class="row d-flex justify-content-between ">
-        <div class="col my-5 d-flex justify-content-start">
-            <a href="/cbdsport" class="">
+    <div class="row">
+        <div class="col-xl-6 my-5 d-flex justify-content-start">
+            <a href="/" class="">
+                
                 <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/menu-logo-svg.svg" alt="" class="header-logo-svg <?php if (get_field("header_dark")): echo "bg-ink-light"; endif; ?>" width="25" height="25">
             </a>
         </div>
-        <div class="col my-5 d-flex justify-content-end">
+        <div class="col-xl-6 my-5 d-flex justify-content-end">
             <div class="header-menu-btn">Menu O</div>
         </div>
     </div>
-    <div class="header-pages d-none">
+    <div class="header-pages">
         <div class="row header-pages-titles">
             <div class="col div-footer-a">
                 <h2 class="header-h2 active"><a href="http://localhost/cbdsport/werk" class="header-a">Werk</a></h2>
@@ -36,13 +37,13 @@ defined( "ABSPATH" ) || exit;
 
         <div class="row header-info">
             <div class="col-sm-3">
-                <h3 class="header-h3 d-none d-sm-block">Contact</h3>
+                <h3 class="header-h3 d-none d-xl-block">Contact</h3>
                 <div class="div-footer-a"><a href="mailto:info@quarterback.nl" class="header-a" title="E-mailadres Quarterback">info@quarterback.nl</a></div>
                 <div class="header-address header-a">Emmaustraat 1</div>
                 <div class="header-address header-a">5492BK Sint-Oedenrode</div>
             </div>
             <div class="col-4 offset-8 col-sm-3 offset-sm-0 text-align-right">
-                <h3 class="header-h3 d-none d-sm-block">Socials</h3>
+                <h3 class="header-h3 d-none d-xl-block">Socials</h3>
                 <div class="div-footer-a"><a href="https://www.instagram.com/quarterback.nl/" class="header-a" target="_blank" title="Instagram Quarterback">Instagram</a></div>
                 <div class="div-footer-a"><a href="https://nl.linkedin.com/in/danny-siebers" class="header-a" target="_blank" title="LinkedIn Quarterback">LinkedIn</a></div>
                 <div class="div-footer-a"><a href="https://www.facebook.com/quarterback.nl/" class="header-a" target="_blank" title="Facebook Quarterback">Facebook</a></div>
