f494a387a6806608710a6514c5baa4b7df4e5b50
Niels Slo <nielssloesen@hotmail.com>
2021-05-21 11:24:47 +0200

    Updated Work page

--- wp-content/themes/inkoppers/template-work.php
+++ wp-content/themes/inkoppers/template-work.php
@@ -9,43 +9,55 @@ get_header();
 
 <div class="container-fluid bg-ink-dark">
     <div class="row">
-        <div class="col">
+        <div class="col-xl-7 offset-xl-1">
             <h1 class="my-5">Wij ontwikkelen concepten en bouwen merken zonder fratsen.</h1>
-            <p class="p-gray mb-5">We create brands that stand out. We are here to tell a story through visual language & brand identity</p>
+         </div>
+         <div class="col-xl-5 offset-xl-1">
+            <p class="p-gray mb-5 mr-xl-5">We create brands that stand out. We are here to tell a story through visual language & brand identity</p>
          </div>
     </div>
 </div>
 
+<div class="container-fluid bg-ink-dark">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="my-3 my-xl-5">
+            <span class="degular-font fs-2440">CBD+Sport</span>
+            <p class="p-gray">The smart athletes choice</p>
+        </div>
+        <div class="col-xl-10 offset-xl-1">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="my-3 my-xl-5">
+            <span class="degular-font fs-2440">CBD+Sport</span>
+            <p class="p-gray">The smart athletes choice</p>
+        </div>
+        <div class="col-xl-10 offset-xl-1">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="my-3 my-xl-5">
+            <span class="degular-font fs-2440">CBD+Sport</span>
+            <p class="p-gray">The smart athletes choice</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid half-dark-half-light">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="my-3 my-xl-5">
+            <span class="degular-font fs-2440">CBD+Sport</span>
+            <p class="p-gray">The smart athletes choice</p>
+        </div>
+    </div>
+</div>
+
 <div class="container-fluid">
-    <div class="row mb-3">
-        <div class="col-12 bg-ink-dark">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
-            <h4 class="mt-4 mb-2">CBD+Sport</h4>
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="my-3 my-xl-5">
+            <span class="degular-font fs-2440">CBD+Sport</span>
             <p class="p-gray">The smart athletes choice</p>
         </div>
-        <div class="col-12 bg-ink-dark">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
-            <h4 class="mt-4 mb-2">CBD+Sport</h4>
-            <p class="p-gray">The smart athletes choice</p>
-        </div>
-        <div class="col-12 bg-ink-dark">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
-            <h4 class="mt-4 mb-2">CBD+Sport</h4>
-            <p class="p-gray">The smart athletes choice</p>
-        </div>
-        <div class="col-12 half-dark-half-light">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
-            <h4 class="mt-4 mb-2">CBD+Sport</h4>
-            <p class="p-gray">The smart athletes choice</p>
-        </div>
-        <div class="col-12">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
-            <h4 class="mt-4 mb-2">CBD+Sport</h4>
-            <p class="p-gray">The smart athletes choice</p>
-        </div>
-        <div class="col-12">
-            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="mt-4">
-            <h4 class="mt-4 mb-2">CBD+Sport</h4>
+        <div class="col-xl-10 offset-xl-1">
+            <img src="http://localhost/cbdsport/wp-content/uploads/2021/05/light-gray.png" alt="" class="my-3 my-xl-5">
+            <span class="degular-font fs-2440">CBD+Sport</span>
             <p class="p-gray">The smart athletes choice</p>
         </div>
     </div>
