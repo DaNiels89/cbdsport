6d171b78786dd0aa4cdd2cd441d408bc7a3844e9
Niels Slo <nielssloesen@hotmail.com>
2021-05-24 20:39:21 +0200

    Added jQuery for accordion on About Us page

--- wp-content/themes/inkoppers/js/custom.js
+++ wp-content/themes/inkoppers/js/custom.js
@@ -1,4 +1,10 @@
-jQuery(document).ready(function ($)
-{
-    //
+jQuery(document).ready(function ($) {
+	// Add class show to Bootstrap accordion on big sizes so it fully opens
+	if ($(window).width() < 960) {
+		// alert("Less than 960");
+	} else {
+		$("#collapseTwo").addClass("show");
+		$("#collapseThree").addClass("show");
+		// alert("More than 960");
+	}
 });
