b9dca675708b35f9bc72f4cebdfd49a0b9576d5b
Niels Slo <nielssloesen@hotmail.com>
2021-05-27 11:59:54 +0200

    Updated styles footer

--- wp-content/themes/inkoppers/footer.php
+++ wp-content/themes/inkoppers/footer.php
@@ -5,30 +5,30 @@ defined("ABSPATH") || exit;
 
 <div id="footer" class="container-fluid <?php if (get_field("footer_dark")): echo "bg-ink-dark"; endif; ?> py-5">
     <div class="row mt-5 py-5 my-xl-5">
-        <div class="col-xl-5 offset-xl-1 my-4">
+        <div class="col-xl-5 offset-xl-1 my-5">
             <span class="degular-font fs-4080">Laten we<br />samen werken.</span>
         </div>
 
         <div class="col-6 col-xl-3 d-flex flex-column">
-            <span class="fs-30 d-none d-xl-block">Contact</span>
-            <p class="mt-xl-5"><a href="mailto:info@quarterback.nl" class="" title="E-mailadres Quarterback">info@quarterback.nl</a></p>
-            <p class="">Emmaustraat 1</p>
-            <p class="">5492BK Sint-Oedenrode</p>
+            <span class="jakarta-medium-font fs-30 d-none d-xl-block">Contact</span>
+            <p class="jakarta-light-font mt-xl-5"><a href="mailto:info@quarterback.nl" class="" title="E-mailadres Quarterback">info@quarterback.nl</a></p>
+            <p class="jakarta-light-font ">Emmaustraat 1</p>
+            <p class="jakarta-light-font ">5492BK Sint-Oedenrode</p>
         </div>
         
         <div class="col-6 col-xl-3 d-flex flex-column">
-            <span class="fs-30 d-none d-xl-block">Socials</span>
-            <p class="mt-xl-5"><a href="https://www.instagram.com/quarterback.nl/" class="" target="_blank" title="Instagram Quarterback">Instagram</a></p>
-            <p><a href="https://nl.linkedin.com/in/danny-siebers" class="" target="_blank" title="LinkedIn Quarterback">LinkedIn</a></p>
-            <p><a href="https://www.facebook.com/quarterback.nl/" class="" target="_blank" title="Facebook Quarterback">Facebook</a></p>
+            <span class="jakarta-medium-font fs-30 d-none d-xl-block">Socials</span>
+            <p class="jakarta-light-font mt-xl-5"><a href="https://www.instagram.com/quarterback.nl/" class="" target="_blank" title="Instagram Quarterback">Instagram</a></p>
+            <p class="jakarta-light-font "><a href="https://nl.linkedin.com/in/danny-siebers" class="" target="_blank" title="LinkedIn Quarterback">LinkedIn</a></p>
+            <p class="jakarta-light-font "><a href="https://www.facebook.com/quarterback.nl/" class="" target="_blank" title="Facebook Quarterback">Facebook</a></p>
         </div>
     </div>
 
-    <div class="row my-xl-4">
+    <div class="row">
         <div class="col col-xl-10 offset-xl-1 d-flex flex-column flex-xl-row">
-            <a href="http://localhost/quarterback/projecten" class="mr-xl-4" title="Projecten Quarterback">Projecten</a>
-            <a href="http://localhost/quarterback/over-ons" class="mx-xl-5" title="Over Quarterback">Over ons</a>
-            <a href="http://localhost/quarterback/contact" class="ml-xl-4" title="Contact Quarterback">Contact</a>
+            <a href="http://localhost/quarterback/projecten" class="mr-xl-4 mb-xl-2" title="Projecten Quarterback">Projecten</a>
+            <a href="http://localhost/quarterback/over-ons" class="mx-xl-5 mb-xl-2" title="Over Quarterback">Over ons</a>
+            <a href="http://localhost/quarterback/contact" class="ml-xl-4 mb-xl-2" title="Contact Quarterback">Contact</a>
         </div>
     </div>
  
@@ -40,11 +40,11 @@ defined("ABSPATH") || exit;
  
      <div class="row">
         <div class="col-xl-5 offset-xl-1">
-            <p class="p-copyright"><a href="/" class="fs-1418">Copyright &copy; 2021 Quarterback</a></p>
+            <p class="p-copyright jakarta-light-font"><a href="/" class="fs-1418">Copyright &copy; 2021 Quarterback</a></p>
         </div>
         <div class="col-xl-5 d-flex justify-content-xl-end">
-            <p class="mr-3"><a class="fs-1418" href="http://localhost/quarterback/algemene-voorwaarden" title="Algemene voorwaarden">Algemene voorwaarden</a></p>
-            <p class=""><a class="fs-1418" href="http://localhost/quarterback/algemene-voorwaarden" title="Privacy">Privacy</a></p>
+            <p class="jakarta-light-font mr-3"><a class="fs-1418" href="http://localhost/quarterback/algemene-voorwaarden" title="Algemene voorwaarden">Algemene voorwaarden</a></p>
+            <p class="jakarta-light-font"><a class="fs-1418" href="http://localhost/quarterback/algemene-voorwaarden" title="Privacy">Privacy</a></p>
         </div>
     </div>
 </div>
