d7a998ec61bcd42d04e5872d37b04cbf3922cd11
Niels Slo <nielssloesen@hotmail.com>
2021-05-28 13:41:17 +0200

    Updated styles home page

--- wp-content/themes/inkoppers/front-page.php
+++ wp-content/themes/inkoppers/front-page.php
@@ -7,7 +7,7 @@ get_header();
 
 <div id="cookie-div" class="container-fluid bg-ink-dark">
     <div class="row py-xl-4">
-        <div class="col col-xl-10 offset-xl-1 d-flex align-items-center justify-content-between">
+        <div class="column col-xl-10 offset-xl-1 d-flex align-items-center justify-content-between">
             <p class="font-manrope p-yellow">Deze site maakt gebruik van cookies, je blijft wel anoniem. Voor meer informatie <a href="#" class="font-manrope a-yellow">Cookie en Privacy Voorwaarden</a></p>
             <button id="cookie-btn" class="font-manrope btn-ink-yellow">OK</button>
         </div>
@@ -54,7 +54,7 @@ get_header();
                 </div>
             </div> -->
         </div>
-        <div class="col col-xl-6 offset-xl-1 background-image-1 min-vh-100">
+        <div class="column col-xl-6 offset-xl-1 background-image-1 min-vh-100">
         </div>
     </div>
 </div>
@@ -62,16 +62,16 @@ get_header();
 <!-- This div is for mobile only -->
 <div class="container-fluid half-light-half-dark d-xl-none">
     <div class="row">
-        <div class="col background-image-2">
+        <div class="column background-image-2">
         </div>       
     </div>
 </div>
 
 <div class="container-fluid">
     <div class="row align-items-center">
-        <div class="col col-xl-4 p-xl-0 d-none d-xl-block background-image-2 h-900">
+        <div class="column col-xl-4 p-xl-0 d-none d-xl-block background-image-2 h-900">
         </div>       
-        <div class="col col-xl-6 offset-xl-1 py-5 p-xl-5">
+        <div class="column col-xl-6 offset-xl-1 py-5 p-xl-5">
             <p class="p-gray jakarta-light-font">Over ons</p>
             <p class="fs-2040 ink-my-4">We zijn nuchtere mensen die geloven in hard werken en in het opbouwen van duurzame relaties. We vinden het leuk om bedrijven er beter uit te laten zien met doeltreffende ontwerpen en heldere communicatie.</p>
 
@@ -88,28 +88,28 @@ get_header();
 
 <div class="container-fluid bg-ink-dark ink-pt-11 ink-pb-11">
     <div class="row">
-        <div class="col col-xl-7 offset-xl-1">
+        <div class="column col-xl-7 offset-xl-1">
             <p class="p-gray jakarta-light-font">Ons werk</p>
             <h2 class="degular-font fs-4080 ink-mt-2 ink-mb-11">Design and development for every stage of your journey.</h2>
         </div>
     </div>
     <div class="row">
-        <div class="col col-xl-5 offset-xl-1">
+        <div class="column col-xl-5 offset-xl-1">
             <div class="background-image-3 h-600 ink-mb-2"></div>
             <span class="degular-font fs-2440">CBD+Sport</span>
             <p class="p-gray jakarta-light-font">The smart athletes choice</p>
         </div>
-        <div class="col col-xl-4 offset-xl-1">
+        <div class="column col-xl-4 offset-xl-1">
             <div class="background-image-4 h-750 ink-mb-2"></div>
             <span class="degular-font fs-2440">CBD+Sport</span>
             <p class="p-gray jakarta-light-font">The smart athletes choice</p>
         </div>
-        <div class="col col-xl-4 offset-xl-1">
+        <div class="column col-xl-4 offset-xl-1">
             <div class="background-image-4 h-750 ink-mb-2"></div>
             <span class="degular-font fs-2440">CBD+Sport</span>
             <p class="p-gray jakarta-light-font">The smart athletes choice</p>
         </div>
-        <div class="col col-xl-5 offset-xl-1 d-flex flex-column align-self-xl-end">
+        <div class="column col-xl-5 offset-xl-1 d-flex flex-column align-self-xl-end">
             <div class="background-image-3 h-600 ink-mb-2"></div>
             <span class="degular-font fs-2440">CBD+Sport</span>
             <p class="p-gray jakarta-light-font">The smart athletes choice</p>
@@ -119,7 +119,7 @@ get_header();
 
 <div class="container-fluid bg-ink-dark">
     <div class="row">
-        <div class="col col-xl-10 offset-xl-1 px-0">
+        <div class="column col-xl-10 offset-xl-1 px-0">
             <hr class="">
         </div>
     </div>
@@ -127,13 +127,13 @@ get_header();
 
 <div class="container-fluid bg-ink-dark ink-pt-11 ink-pb-14">
     <div class="row">
-        <div class="col col-xl-7 offset-xl-1 px-xl-0">
+        <div class="column col-xl-7 offset-xl-1 px-xl-0">
             <p class="p-gray jakarta-light-font">Wat we doen</p>
             <h2 class="degular-font fs-4080 ink-mt-2 ink-mb-5">Design and development for every stage of your journey.</h2>
         </div> 
     </div>
     <div class="row">
-        <div class="col col-xl-10 offset-xl-1">
+        <div class="column col-xl-10 offset-xl-1">
             <div class="row">
                 <div class="col-4 pl-0 pr-2">
                     <span class="degular-font fs-2440">Concept</span>
@@ -154,7 +154,7 @@ get_header();
 
 <div class="container-fluid ink-pt-12 ink-pb-4">
     <div class="row my-5">
-        <div class="col col-xl-10 offset-xl-1">
+        <div class="column col-xl-10 offset-xl-1">
             <div class="row">
                 <?php
                     $query = new WP_Query(["post_status" => "publish", "posts_per_page" => 8, "post_type" => "customers", "orderby" => "ID", "order" => "ASC"]);
@@ -164,7 +164,7 @@ get_header();
                         $klantenlogo = get_field('klantenlogo');
                         $website = get_field('website');
                 ?>
-                    <div class="col col-6 col col-md-3 py-5 d-flex justify-content-center align-items-center">
+                    <div class="column col-6 col col-md-3 py-5 d-flex justify-content-center align-items-center">
                         <a href="<?php echo $website ?>" class="" target="_blank">
                             <img src="<?php echo $klantenlogo['sizes']['large']; ?>" alt="<?php echo $klantenlogo['alt']; ?>" />
                         </a>
@@ -177,7 +177,7 @@ get_header();
         </div>
     </div>
     <div class="row my-5">
-        <div class="col my-5 d-flex align-items-center justify-content-center">
+        <div class="column col-12 my-5 d-flex align-items-center justify-content-center">
             <a href="http://localhost/quarterback/klanten" class="a-showreal mr-4">Meer klanten</a>
             <a href="/quarterback/klanten" class="">
                 <svg id="Group_812" data-name="Group 812" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
@@ -192,19 +192,19 @@ get_header();
 <!-- This div is for mobile only -->
 <div class="container-fluid half-light-half-dark d-xl-none">
     <div class="row">
-        <div class="col background-image">
+        <div class="column background-image">
         </div>       
     </div>
 </div>
 
 <div class="container-fluid">
     <div class="row align-items-center">
-        <div class="col col-xl-4 p-xl-0 d-none d-xl-block background-image-2 h-900">
+        <div class="column col-xl-4 p-xl-0 d-none d-xl-block background-image-2 h-900">
         </div>       
-        <div class="col col-xl-6 offset-xl-1 py-5 p-xl-5">
+        <div class="column col-xl-6 offset-xl-1 py-5 p-xl-5">
 
             <span class="degular-font fs-4080 my-5">Helping companies prototype & Launch their ideas faster.</span>
-            <div class="col my-5 d-flex align-items-center px-0">
+            <div class="column my-5 d-flex align-items-center px-0">
                 <a href="http://localhost/quarterback/contact" class="a-showreal mr-4">Neem contact op</a>
                 <a href="/quarterback/contact" class="">
                     <svg id="Group_812" data-name="Group 812" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
