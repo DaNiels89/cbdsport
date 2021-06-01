693ab28b3437bebb4715b44c5bd8f0abd981fb95
Niels Slo <nielssloesen@hotmail.com>
2021-06-01 11:54:14 +0200

    Build content page desktop and mobile

--- wp-content/themes/inkoppers/template-content.php
+++ wp-content/themes/inkoppers/template-content.php
@@ -1,5 +1,5 @@
 <?php
-/* Template Name: Contact */ 
+/* Template Name: Content */ 
 
 // Exit if accessed directly.
 defined("ABSPATH") || exit;
@@ -7,10 +7,72 @@ defined("ABSPATH") || exit;
 get_header();
 ?>
 
+<div class="container-fluid upper-container">
+    <div class="row">
+        <div class="col-xl-4 offset-xl-2">
+            <h1 class="">Wij ontwikkelen concepten en bouwen merken</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid ink-my-4 mx-0">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 background-image-contact">
+
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid ink-mt-8 mx-0">
+    <div class="row">
+        <div class="col-xl-8 offset-xl-2">
+            <p class="fs-2040">Wij zijn gespecialiseerd in het bouwen van merken, het ontwikkelen van producten en het vertellen van visuele verhalen. We zijn een klein, gedreven team van creatievelingen die identiteiten ontwerpen, geleverd met verfrissende eenvoud.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid ink-my-4 mx-0">
+    <div class="row">
+        <div class="col-xl-8 offset-xl-2">
+            <p class="jakarta-light-font fs-1825 lh-175">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="column col-xl-6 offset-xl-2">
+            <p class="p-gray jakarta-light-font pt-5">Wat we doen</p>
+            <h2 class="degular-font fs-4080 ink-mb-2">Design and development for every stage of your journey.</h2>
+        </div> 
+    </div>
+    <div class="row">
+        <div class="column col-xl-8 offset-xl-2">
+            <div class="row">
+                <div class="col-xl-5 pr-xl-2">
+                    <span class="degular-font fs-2440">Concept</span>
+                    <p class="jakarta-light-font fs-1825 ink-my-2 mx-0 lh-175">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+                </div>
+                <div class="col-xl-5 offset-xl-1 pl-xl-0 pr-xl-2">
+                    <span class="degular-font fs-2440">Branding</span>
+                    <p class="jakarta-light-font fs-1825 ink-my-2 mx-0 lh-175">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container-fluid bg-ink-dark">
     <div class="row">
-        <div class="col">
-            Content
+        <div class="col-xl-8 offset-xl-2">
+            <div class="row ink-my-5 mx-0">
+                <div class="col-xl-5 background-image-content px-0">
+
+                </div>
+                <div class="col-xl-5 offset-xl-1 px-0 d-flex align-items-center">
+                    <p class="jakarta-light-font fs-1825 lh-175 ink-mt-3 ink-mt-lg-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
