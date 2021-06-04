7689dcefbeab7963ff07d7fc3547a582a8f4a8c1
Niels Slo <nielssloesen@hotmail.com>
2021-06-04 11:46:02 +0200

    Updated contact page

--- wp-content/themes/inkoppers/footer.php
+++ wp-content/themes/inkoppers/footer.php
@@ -3,30 +3,39 @@
 defined("ABSPATH") || exit;
 ?>
 
+<?php 
+    $footerTitle = get_field("footer_title", "option");
+    $contactTitle = get_field("contact_title", "option");
+    $contactEmail = get_field("contact_email_address", "option");
+    $contactStreet = get_field("contact_street", "option");
+    $contactZipCity = get_field("contact_zip_code_city", "option");
+    $socialsTitle = get_field("socials_title", "option");
+    $socialInstagram = get_field("social_instagram", "option");
+    $socialLinkedIn = get_field("social_linkedin", "option");
+    $socialFacebook = get_field("social_facebook", "option");
+    $footerHomeLink = get_field("footer_home_link");
+    $footerProjectsLink = get_field("footer_projects_link");
+    $footerAboutUsLink = get_field("footer_about_us_link");
+    $footerContactLink = get_field("footer_contact_link");
+    $footerCopyright = get_field("footer_copyright", "option");
+    $footerTermsAndConditions = get_field("footer_terms_and_conditions", "option");
+    $footerPrivacy = get_field("footer_privacy", "option" );
+
+?>
+
 <div id="footer" class="qb-footer container-fluid <?php if (get_field("footer_dark")): echo "bg-ink-dark"; endif; ?> pb-5 py-xl-5">
     <?php if (!is_page( 30 )): ?> 
     <div class="row mt-xl-5 pb-5 py-xl-5 my-xl-5 ">
         <div class="col-xl-5 offset-xl-1 my-5">
-            <?php 
-                $footerTitle = get_field("footer_title", "option")
-            ?>
             <?php if ($footerTitle): ?>
                 <span class="degular-font fs-4080"><?php echo $footerTitle; ?></span>
             <?php endif; ?>
         </div>
 
         <div class="col-7 col-xl-3 d-flex flex-column">
-             <?php 
-                $contactTitle = get_field("contact_title", "option")
-            ?>
             <?php if ($contactTitle): ?>
                 <span class="jakarta-medium-font fs-30 d-none d-xl-block"><?php echo $contactTitle; ?></span>
             <?php endif; ?>        
-            <?php
-                $contactEmail = get_field("contact_email_address", "option");
-                $contactStreet = get_field("contact_street", "option");
-                $contactZipCity = get_field("contact_zip_code_city", "option");
-            ?>
             <?php if ($contactEmail): ?>
                 <p class="jakarta-light-font mt-xl-5"><a href="mailto:<?php echo $contactEmail; ?>" class="a-hover-underline-arrow" title="E-mailadres Quarterback"><?php echo $contactEmail; ?></a></p>
             <?php endif; ?>
@@ -39,17 +48,9 @@ defined("ABSPATH") || exit;
         </div>
         
         <div class="col-5 col-xl-3 d-flex flex-column">
-            <?php 
-                $socialsTitle = get_field("socials_title", "option")
-            ?>
             <?php if ($socialsTitle): ?>
                 <span class="jakarta-medium-font fs-30 d-none d-xl-block"><?php echo $socialsTitle; ?></span>
             <?php endif; ?> 
-            <?php
-                $socialInstagram = get_field("social_instagram", "option");
-                $socialLinkedIn = get_field("social_linkedin", "option");
-                $socialFacebook = get_field("social_facebook", "option");
-            ?>
             <?php if ($socialInstagram): ?>
                 <p class="jakarta-light-font a-hover-underline-arrow mt-xl-5"><a href="<?php echo $socialInstagram["url"]; ?>" class="" target="<?php echo $socialInstagram["target"]; ?>" title="<?php echo $socialInstagram["title"]; ?>"><?php echo $socialInstagram["title"]; ?></a></p>
             <?php endif; ?>
@@ -64,12 +65,6 @@ defined("ABSPATH") || exit;
     <?php endif;?>
     <div class="row">
         <div class="col col-xl-10 offset-xl-1 d-flex flex-column flex-xl-row my-3">
-            <?php
-                $footerHomeLink = get_field("footer_home_link");
-                $footerProjectsLink = get_field("footer_projects_link");
-                $footerAboutUsLink = get_field("footer_about_us_link");
-                $footerContactLink = get_field("footer_contact_link");
-            ?>
             <a href="<?php echo get_site_url(); ?>/projecten" class="a-hover-underline-arrow mr-xl-4 mb-xl-2" title="Projecten Quarterback">Projecten</a>
             <a href="<?php echo get_site_url(); ?>/over-ons" class="a-hover-underline-arrow mx-xl-5 mb-xl-2" title="Over Quarterback">Over ons</a>
             <a href="<?php echo get_site_url(); ?>/contact" class="a-hover-underline-arrow ml-xl-4 mb-xl-2" title="Contact Quarterback">Contact</a>
@@ -84,18 +79,11 @@ defined("ABSPATH") || exit;
  
      <div class="row">
         <div class="col-xl-5 offset-xl-1">
-            <?php 
-                $footerCopyright = get_field("footer_copyright", "option");
-            ?>
             <?php if ($footerCopyright): ?>
                 <p class="p-copyright jakarta-light-font"><a href="<?php echo get_site_url(); ?>" class="fs-1418"><?php echo $footerCopyright; ?></a></p>
             <?php endif;?>
         </div>
         <div class="col-xl-5 d-flex justify-content-xl-end">
-            <?php
-                $footerTermsAndConditions = get_field("footer_terms_and_conditions", "option");
-                $footerPrivacy = get_field("footer_privacy", "option" );
-            ?>
             <?php if ($footerTermsAndConditions): ?>
                 <p class="jakarta-light-font mr-3"><a class="fs-1418" href="<?php echo $footerTermsAndConditions["url"]; ?>" target="<?php echo $footerTermsAndConditions["target"]; ?>" title="<?php echo $footerTermsAndConditions["title"]; ?>"><?php echo $footerTermsAndConditions["title"]; ?></a></p>
             <?php endif;?>
--- wp-content/themes/inkoppers/template-contact.php
+++ wp-content/themes/inkoppers/template-contact.php
@@ -6,15 +6,23 @@ defined("ABSPATH") || exit;
 
 get_header();
 ?>
+<?php 
+    $footerTitle = get_field("footer_title", "option");
+    $contactTitle = get_field("contact_title", "option");
+    $contactEmail = get_field("contact_email_address", "option");
+    $contactStreet = get_field("contact_street", "option");
+    $contactZipCity = get_field("contact_zip_code_city", "option");
+    $socialsTitle = get_field("socials_title", "option");
+    $socialInstagram = get_field("social_instagram", "option");
+    $socialLinkedIn = get_field("social_linkedin", "option");
+    $socialFacebook = get_field("social_facebook", "option");
+?>
 
 <div class="container-fluid qb-footer bg-ink-dark">
     <div class="row">
         <div class="col-xl-6 background-image-contact mvh-mobile-70 mvh-desktop-80 position-relative">
             <div class="row">
                 <div class="col-12 offset-xl-2 position-absolute contact-page-footer">
-                    <?php 
-                        $footerTitle = get_field("footer_title", "option")
-                    ?>
                     <?php if ($footerTitle): ?>
                         <span class="degular-font fs-4080"><?php echo $footerTitle; ?></span>
                     <?php endif; ?>
@@ -24,16 +32,32 @@ get_header();
         <div class="col-xl-6 position-relative">
             <div class="row d-flex h-100 justify-content-center align-items-end ink-py-6 px-0">
                 <div class="col-7 col-xl-6 offset-xl-1 d-flex flex-column">
-                    <span class="jakarta-medium-font fs-30 d-none d-xl-block">Contact</span>
-                    <p class="jakarta-light-font mt-xl-5"><a href="mailto:info@quarterback.nl" class="a-hover-underline-arrow" title="E-mailadres Quarterback">info@quarterback.nl</a></p>
-                    <p class="jakarta-light-font a-hover-underline-arrow">Emmaustraat 1</p>
-                    <p class="jakarta-light-font a-hover-underline-arrow">5492BK Sint-Oedenrode</p>
+                    <?php if ($contactTitle): ?>
+                        <span class="jakarta-medium-font fs-30 d-none d-xl-block"><?php echo $contactTitle; ?></span>
+                    <?php endif; ?>        
+                    <?php if ($contactEmail): ?>
+                        <p class="jakarta-light-font mt-xl-5"><a href="mailto:<?php echo $contactEmail; ?>" class="a-hover-underline-arrow" title="E-mailadres Quarterback"><?php echo $contactEmail; ?></a></p>
+                    <?php endif; ?>
+                    <?php if ($contactStreet): ?>
+                        <p class="jakarta-light-font a-hover-underline-arrow"><?php echo $contactStreet; ?></p>
+                    <?php endif; ?>
+                    <?php if ($contactZipCity): ?>
+                        <p class="jakarta-light-font a-hover-underline-arrow"><?php echo $contactZipCity; ?></p>
+                    <?php endif; ?>
                 </div>
                 <div class="col-5 col-xl-4 d-flex flex-column">
-                    <span class="jakarta-medium-font fs-30 d-none d-xl-block">Socials</span>
-                    <p class="jakarta-light-font a-hover-underline-arrow mt-xl-5"><a href="https://www.instagram.com/quarterback.nl/" class="" target="_blank" title="Instagram Quarterback">Instagram</a></p>
-                    <p class="jakarta-light-font a-hover-underline-arrow"><a href="https://nl.linkedin.com/in/danny-siebers" class="" target="_blank" title="LinkedIn Quarterback">LinkedIn</a></p>
-                    <p class="jakarta-light-font a-hover-underline-arrow"><a href="https://www.facebook.com/quarterback.nl/" class="" target="_blank" title="Facebook Quarterback">Facebook</a></p>
+                    <?php if ($socialsTitle): ?>
+                        <span class="jakarta-medium-font fs-30 d-none d-xl-block"><?php echo $socialsTitle; ?></span>
+                    <?php endif; ?> 
+                    <?php if ($socialInstagram): ?>
+                        <p class="jakarta-light-font a-hover-underline-arrow mt-xl-5"><a href="<?php echo $socialInstagram["url"]; ?>" class="" target="<?php echo $socialInstagram["target"]; ?>" title="<?php echo $socialInstagram["title"]; ?>"><?php echo $socialInstagram["title"]; ?></a></p>
+                    <?php endif; ?>
+                    <?php if ($socialLinkedIn): ?>
+                        <p class="jakarta-light-font a-hover-underline-arrow"><a href="<?php echo $socialLinkedIn["url"]; ?>" class="" target="<?php echo $socialLinkedIn["target"]; ?>" title="<?php echo $socialLinkedIn["title"]; ?>"><?php echo $socialLinkedIn["title"]; ?></a></p>
+                    <?php endif; ?>
+                    <?php if ($socialFacebook): ?>
+                        <p class="jakarta-light-font a-hover-underline-arrow"><a href="<?php echo $socialFacebook["url"]; ?>" class="" target="<?php echo $socialFacebook["target"]; ?>" title="<?php echo $socialFacebook["title"]; ?>"><?php echo $socialFacebook["title"]; ?></a></p>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
